b75005773051014a55e7647adc8ff6278aff55e2
infinitedim <saputradimas.boediluhur@gmail.com>
2022-03-06 10:42:26 +0700

    hello.ts

---
+++ hello.js
@@ -0,0 +1,5 @@
+function sayHello(name, age) {
+    return "Hello ".concat(name, ", im ").concat(age, " years old");
+}
+var hello = sayHello("Dimas", 17);
+console.log(hello);
---
+++ hello.ts
@@ -0,0 +1,8 @@
+function sayHello(name: string, age: number) {
+  return `Hello ${name}, im ${age} years old`;
+}
+
+let hello: string = sayHello("Dimas", 17)
+
+console.log(hello)
+ 
