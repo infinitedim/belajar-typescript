dc72ce580aa94155dc66c4f5d93bcc5c5eda03e0
dimasgit <aansaputra8890@gmail.com>
2022-03-08 13:04:39 +0700

    adding object data type

---
+++ data-types/object.js
@@ -0,0 +1,15 @@
+var employee;
+employee = {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 30,
+    jobTitle: 'Web Developer'
+};
+var employee2 = {
+    firstName: "Dimas",
+    lastName: "Saputra",
+    age: 17,
+    jobTitle: "Front End Web Developer",
+    haveAJob: true
+};
+console.log(employee2);
---
+++ data-types/object.ts
@@ -0,0 +1,26 @@
+let employee: object;
+
+employee = {
+    firstName: 'John',
+    lastName: 'Smith',
+    age: 30,
+    jobTitle: 'Web Developer',
+};
+
+
+
+let employee2: {
+    firstName: string,
+    lastName: string,
+    age: number,
+    jobTitle: string,
+    haveAJob: boolean
+} = {
+    firstName: "Dimas",
+    lastName: "Saputra",
+    age: 17,
+    jobTitle: "Front End Web Developer",
+    haveAJob: true
+}
+
+console.log(employee2)
